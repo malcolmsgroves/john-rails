62c82fe5e71536c104fdc89a67bc7139eb017105
Malcolm Groves <macgroves@gmail.com>
2018-01-10 17:40:47 -0500

    Geocoder up and running

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -8,8 +8,9 @@ class Toilet < ApplicationRecord
   self.per_page = 10
 
   def Toilet.close_to(location)
-    toilet_ids = Location.near([location.lat, location.lng], 1000).select("id").limit(50).map(&:toilet_id)
-    @toilets = Toilet.includes(:location).where(:id => toilet_ids)
+    # toilet_ids = Location.near([location.lat, location.lng], 1000).select("id").limit(50).map(&:toilet_id)
+    # @toilets = Toilet.includes(:location).where(:id => toilet_ids)
+    @toilets = Location.near([location.lat, location.lng], 1000).joins(:toilet).limit(50).map { |loc| loc.toilet }
   end
 
 end
